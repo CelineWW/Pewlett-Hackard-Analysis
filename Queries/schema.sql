034e999d6f908b18a1acf648d79780d9b918d84c
CelineWW <wzhj.ce@gmail.com>
2022-07-09 20:04:56 -0700

    Adding challenge files

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -50,7 +50,7 @@ CREATE TABLE dept_emp (
 --DROP TABLE titles CASCADE; 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR(50) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
